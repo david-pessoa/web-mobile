1b7bb15b6487aa0e49bc475712319718927ce306
david-pessoa <pessoa.dvlbp@gmail.com>
2025-09-03 18:32:09 -0300

    finaliza seção de depoimentos

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
     
+    const listaDepoimentos = document.getElementById('listaDepoimentos');
+
+    fetch('./depoimentos.json')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Erro ao carregar o JSON: " + response.status);
+          }
+          return response.json();
+        })
+        .then(data => {
+            console.log("Dados carregados:", data);
+
+            // Carrega Boxes com os depoimentos dinamicamente
+            data.forEach((depoimento) => {
+            const item = document.createElement("li");
+            item.className = "dark-background";
+            item.innerHTML = `
+                <span class="quotes">“</span>
+		        <blockquote>
+		        	${depoimento.texto}
+		        </blockquote>
+		        <span class="quotes below">”</span>
+		        <div class="name">${depoimento.pessoa}</div>
+            `;
+            listaDepoimentos.appendChild(item);
+            });
+        })
+        .catch(error => console.error(error));
+
+    
+    
+
 });
