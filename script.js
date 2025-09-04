d6bcc527e373e5ebcfb611dc26d81af9848f61e8
david-pessoa <pessoa.dvlbp@gmail.com>
2025-09-04 01:01:20 -0300

    substitui div por tag cite, coloca estrelas nas avaliações. Falta carrossel

--- script.js
+++ script.js
@@ -2,32 +2,48 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const listaDepoimentos = document.getElementById('listaDepoimentos');
 
+    // Obtém dados do JSON com os depoimentos
     fetch('./depoimentos.json')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error("Erro ao carregar o JSON: " + response.status);
-          }
-          return response.json();
-        })
-        .then(data => {
-            console.log("Dados carregados:", data);
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Erro ao carregar o JSON: " + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+          // Carrega Boxes com os depoimentos dinamicamente
+          const testData = [data[0], data[1], data[2]]; //Apenas usando um array de teste enquanto não usa um carrossel
+          testData.forEach((depoimento) => {
+          const item = document.createElement("li");
+          item.className = "dark-background";
+          
+          // Adiciona as estrelas
+          let estrelas = depoimento.estrelas;
+          let starLine = "<span class='stars'>";
+          for(let i = 0; i < 5; i++)
+          {
+            if(estrelas > 0)
+              starLine += '<i class="lni-star-filled"></i>';
+            else
+              starLine += '<i class="lni-star-half"></i>'
 
-            // Carrega Boxes com os depoimentos dinamicamente
-            data.forEach((depoimento) => {
-            const item = document.createElement("li");
-            item.className = "dark-background";
-            item.innerHTML = `
-                <span class="quotes">“</span>
+            estrelas--;
+          }
+          starLine += "</span>";
+          
+          // Adiciona depoimento
+          item.innerHTML = `
+            <span class="quotes">“</span>
 		        <blockquote>
 		        	${depoimento.texto}
 		        </blockquote>
-		        <span class="quotes below">”</span>
-		        <div class="name">${depoimento.pessoa}</div>
-            `;
-            listaDepoimentos.appendChild(item);
-            });
-        })
-        .catch(error => console.error(error));
+            <span class="quotes below">”</span>
+            <cite>${depoimento.pessoa}</cite>
+          ` + starLine;
+          listaDepoimentos.appendChild(item);
+          });
+      })
+      .catch(error => console.error(error));
 
     
     
